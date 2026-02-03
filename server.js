aabd9936c7ec8f341da0b0e0f04ff4cfb735c294
Ishank307 <ishankkumar307@gmail.com>
2026-02-03 15:46:35 +0530

    feat: added mongoose models and database connections

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app= require('./src/app')
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.listen("3000",(req,res)=>{
-    console.log("server is runnning on port 3000")
-})
+const app= require("./src/app");
+const connectDB= require("./src/database/db")
+
+const PORT = process.env.PORT || 3000
+
+// when application gets connected to databse only then switch on the server
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to database", err.message);
+  });
---
+++ src/models/Movie.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+// Movie schema representing basic movie metadata
+
+const movieSchema = mongoose.Schema({
+    title: {
+        type: String,
+        Required: true,
+        trim: true
+    },
+    description:{
+        type:"String",
+        trim:true,
+    },
+    genre:{
+        type:"String",
+        trim:true
+    },
+    releaseYear:{
+        type:Number
+    },
+},
+{
+  timestamps:true  
+}
+)
+
+module.exports=mongoose.model("movieSchema")
